a3c791a1706b49d51aee98794b4a0abdf0f2f2de
wenqf11 <qingfu.wen@gmail.com>
2017-01-26 16:23:36 +0800

    73

---
+++ 73. Set Matrix Zeroes/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "solution.h"
+
+
+int main()
+{
+	Solution s;
+	vector<vector<int>> nums = { { 1,2,0 },{ 4,5,6 } };
+	s.setZeroes(nums);
+	system("pause");
+	return 0;
+}
