08f99d1cf430cce325839d3cb645c96e9f55907a
wenqf11 <qingfu.wen@gmail.com>
2017-02-17 23:16:54 +0800

    100

---
+++ 100. Same Tree/solution.h
@@ -0,0 +1,36 @@
+#pragma once
+#include<vector>
+#include<string>
+#include<cstring>
+#include<climits>
+#include<algorithm>
+#include<cmath>
+#include<stack> 
+#include <numeric>
+#include<unordered_map>
+#include<unordered_set>
+#include<sstream>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		if (p == NULL && q == NULL) return true;
+		if (p == NULL && q != NULL) return false;
+		if (p != NULL && q == NULL) return false;
+
+		if (p->val != q->val) {
+			return false;
+		}
+		
+		return isSameTree(p->left, q->left) & isSameTree(p->right, q->right);
+	}
+};
