dcb5f32100d13242a822dcb5bbeaa7d640de1b0e
wenqingfu <qingfu.wen@gmail.com>
2017-03-13 20:43:39 +0800

    132

---
+++ 132. Palindrome Partitioning II/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "solution.h"
+
+// Unit Test
+
+int main() {
+	Solution s;
+	cout << s.minCut("fifgbeajcacehiicccfecbfhhgfiiecdcjjffbghdidbhbdbfbfjccgbbdcjheccfbhafehieabbdfeigbiaggchaeghaijfbjhi") << endl;
+	return 0;
+}
