13defcd174b2d2cba38c16140d75bb16cd83d54b
wenqingfu <qingfu.wen@gmail.com>
2017-04-05 22:09:53 +0800

    153

---
+++ 153. Find Minimum in Rotated Sorted Array/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "solution.h"
+
+// Unit Test
+
+int main() {
+	Solution s;
+	vector<int> nums;
+	nums.push_back(2);
+	nums.push_back(3);
+	nums.push_back(1);
+	nums.push_back(2);
+	//vector< vector<int> > a = {{1, 3, 1 }, {1, 5, 1 }, { 4, 2, 1 }};
+	cout << s.findMin(nums);
+	return 0;
+}
---
+++ 153. Find Minimum in Rotated Sorted Array/solution.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+#include <queue>
+#include <string>
+#include <climits>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int findMin(vector<int>& nums) {
+		int left = 0, right = nums.size() - 1;
+		if (nums.size() == 0) return 0;
+		int mid = 0;
+		while (left <= right) {
+			mid = (left + right) / 2;
+			if (nums[mid] > nums[left] && nums[right] <= nums[left]) {
+				left = mid;
+			} else if (nums[mid] < nums[left]) {
+				right = mid;
+			} else {
+				return min(nums[left], nums[right]);
+			}
+		}
+		return nums[mid];
+	}
+};
