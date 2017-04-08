76c02ba1c6a119a5f29ef548444354acdf33d460
wenqingfu <qingfu.wen@gmail.com>
2017-04-08 13:23:33 +0800

    i167

---
+++ 167. Two Sum II - Input array is sorted/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "solution.h"
+
+// Unit Test
+
+int main() {
+	Solution s;
+	vector<int> nums;
+	nums.push_back(1);
+	nums.push_back(2);
+	s.twoSum(nums, 3);
+	return 0;
+}
