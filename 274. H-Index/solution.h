dcfd256dd4abdf802f9c32982b210f47559f5adb
wenqingfu <qingfu.wen@gmail.com>
2017-05-20 20:48:28 +0800

    275

--- 274. H-Index/solution.h
+++ 274. H-Index/solution.h
@@ -11,14 +11,19 @@ using namespace std;
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end());
+        vector<int> f(citations.size() + 1);
         int n = citations.size();
         for (int i = 0; i < n; i++) {
-            if (citations[i] >= n - i) {
-                return n - i;
-            }
-
+            f[citations[i] <= n ? citations[i] : n] += 1;
         }
-        return 0;
+
+        int sum = 0;
+        for (int i = n; i > 0; i--) {
+            sum += f[i];
+            if (sum >= i) {
+                return i;
+            }
+        }
+        return 1;
     }
 };
