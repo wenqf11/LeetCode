bd68b935754edc27e92b5f306f17d36657de4a57
wenqingfu <qingfu.wen@gmail.com>
2017-06-14 18:39:23 +0800

    350

---
+++ C++/350. Intersection of Two Arrays II/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "solution.h"
+
+int main() {
+    Solution s;
+    vector<int> nums;
+    vector<int> nums2;
+
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(3);
+    nums.push_back(2);
+    nums2.push_back(2);
+    nums2.push_back(2);
+    nums2.push_back(1);
+    nums2.push_back(1);
+
+    s.intersect(nums, nums2);
+
+}
---
+++ C++/350. Intersection of Two Arrays II/solution.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include <queue>
+#include <string>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> m;
+        vector<int> res;
+        for (auto a : nums1) ++m[a];
+        for (auto a : nums2) {
+            if (m[a]-- > 0) res.push_back(a);
+        }
+        return res;
+    }
+};
