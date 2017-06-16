775e2408c3b0346ae9beeb911630534b0b006626
wenqingfu <qingfu.wen@gmail.com>
2017-06-16 10:30:43 +0800

    372

---
+++ C++/372. Super Pow/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "solution.h"
+
+int main() {
+    Solution s;
+    vector<int> b;
+    b.push_back(1);
+    b.push_back(2);
+    cout << s.superPow(3, b) << endl;
+
+}
