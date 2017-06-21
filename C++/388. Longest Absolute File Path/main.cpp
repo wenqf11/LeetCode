81c65d2b8812cf20c271de28b41e203538c6617a
wenqingfu <qingfu.wen@gmail.com>
2017-06-21 21:51:00 +0800

    388

---
+++ C++/388. Longest Absolute File Path/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "solution.h"
+
+int main() {
+    Solution s;
+    cout << s.lengthLongestPath("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext") << endl;
+}
---
+++ C++/388. Longest Absolute File Path/solution.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+#include <queue>
+#include <string>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <unordered_map>
+#include <sstream>
+using namespace std;
+
+class Solution {
+public:
+    int lengthLongestPath(string input) {
+        int res = 0;
+        istringstream ss(input);
+        unordered_map<int, int> m;
+        m[0] = 0;
+        string line = "";
+        while (getline(ss, line)) {
+            int level = line.find_last_of('\t') + 1;
+            int len = line.substr(level).size();
+            if (line.find('.') != string::npos) {
+                res = max(res, m[level] + len);
+            } else {
+                m[level + 1] = m[level] + len + 1;
+            }
+        }
+        return res;
+    }
+};
