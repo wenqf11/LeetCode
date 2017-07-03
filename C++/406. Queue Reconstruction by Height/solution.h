e250bda85ef4a07777cf0f649d6cd0d4751b4c51
wenqingfu <qingfu.wen@gmail.com>
2017-07-03 22:21:17 +0800

    406

---
+++ C++/406. Queue Reconstruction by Height/solution.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <queue>
+#include <string>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
+        sort(people.begin(), people.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+            return a.first > b.first || (a.first == b.first && a.second < b.second);
+        });
+        vector<pair<int, int>> res;
+        for (auto a : people) {
+            res.insert(res.begin() + a.second, a);
+        }
+        return res;
+    }
+};
