6f2eff8fe7600c5ec902f0b749b9a7442d592c92
wenqingfu <qingfu.wen@gmail.com>
2017-07-07 17:51:29 +0800

    413

--- C++/413. Arithmetic Slices/main.cpp
+++ C++/413. Arithmetic Slices/main.cpp
@@ -8,5 +8,6 @@ int main() {
     nums.push_back(2);
     nums.push_back(3);
     nums.push_back(4);
+    nums.push_back(5);
     cout << s.numberOfArithmeticSlices(nums) << endl;
 }
--- C++/413. Arithmetic Slices/solution.h
+++ C++/413. Arithmetic Slices/solution.h
@@ -12,6 +12,16 @@ using namespace std;
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& A) {
-
+        int res = 0, len = 2, n = A.size();
+        for (int i = 2; i < n; ++i) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                ++len;
+            } else {
+                if (len > 2) res += (len - 1) * (len - 2) * 0.5;
+                len = 2;
+            }
+        }
+        if (len > 2) res += (len - 1) * (len - 2) * 0.5;
+        return res;
     }
 };
