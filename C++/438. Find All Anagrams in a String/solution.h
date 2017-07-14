33e541f0722ca8c763e99d7b01ee6d235477ba9f
wenqingfu <qingfu.wen@gmail.com>
2017-07-14 23:06:35 +0800

    438

---
+++ C++/438. Find All Anagrams in a String/solution.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include <queue>
+#include <string>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <unordered_map>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        if (s.empty()) return {};
+        vector<int> res, m(256, 0);
+        int left = 0, right = 0, cnt = p.size(), n = s.size();
+        for (char c : p) ++m[c];
+        while (right < n) {
+            if (m[s[right++]]-- >= 1) --cnt;
+            if (cnt == 0) res.push_back(left);
+            if (right - left == p.size() && m[s[left++]]++ >= 0) ++cnt;
+        }
+        return res;
+    }
+};
