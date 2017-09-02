dffbe049d07745c9da4c59ec80a943cf5c0e213e
wenqingfu <qingfu.wen@gmail.com>
2017-09-02 12:38:27 +0800

    529

---
+++ C++/529. Minesweeper/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "solution.h"
+
+int main() {
+    solution s;
+    vector<vector<char>> board;
+    vector<int> click;
+    s.updateBoard(board, click);
+    return 0;
+}
