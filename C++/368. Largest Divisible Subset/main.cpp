a8a717c5e5556677569a28047b18b40d763e36c9
wenqingfu <qingfu.wen@gmail.com>
2017-09-19 17:12:34 +0800

    368

---
+++ C++/368. Largest Divisible Subset/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "solution.h"
+
+int main() {
+    Solution s;
+    vector<int> nums;
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(4);
+    vector<int> ans = s.largestDivisibleSubset(nums);
+    cout << ans.size() << endl;
+    return 0;
+}
